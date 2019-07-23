7d458171f973830a97ff76e6d72783ce38af31a2
ann.ko <ann.ko@368media.com>
2019-07-23 14:47:14 +0800

    修改樣式

--- FrontEnd/src/app/views/big-data/big-data.component.ts
+++ FrontEnd/src/app/views/big-data/big-data.component.ts
@@ -11,6 +11,10 @@ export class BigDataComponent implements OnInit {
 
   TextAreaMessage: any[] = [];
   socket : any;
+  name : string;
+  self : boolean;
+  chatstyle : any;
+  
   ngOnInit() {
     // Create WebSocket connection.
     this.socket = new WebSocket('ws://127.0.0.1:8080', 'echo-protocol');
@@ -24,11 +28,17 @@ export class BigDataComponent implements OnInit {
     this.socket.onerror = (error) => {
       console.log(`WebSocket error : ${error}`);
     }
+
+    this.chatstyle= {
+      height: '50%',
+      overflow : 'scroll'
+    }
   }
 
   /*Client WebSocket*/
   ConnectWebSocket (message:string ,name:string){
     console.log('Inserted data is =>', message);
+    this.name = name;
     const sendingMessages = {
       name : name,
       message : message
@@ -44,6 +54,11 @@ export class BigDataComponent implements OnInit {
     this.socket.onmessage = (event) => {
       console.log('Received data from server is =>', event.data);
       let receivedMsg = JSON.parse(event.data);
+      if(receivedMsg.name !== this.name){
+        this.self = false;
+      } else if(receivedMsg.name === this.name) {
+        this.self = true;
+      }
       this.TextAreaMessage.push({"Name": receivedMsg.name, "Message": receivedMsg.message});
     }
   }
