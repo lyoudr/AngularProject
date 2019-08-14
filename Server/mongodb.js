c85ceb8cc0d807b0b41029f2b040816aa63c3458
ann.ko <ann.ko@368media.com>
2019-08-14 11:38:47 +0800

    add mongodb explain

---
+++ Server/mongodb.js
@@ -0,0 +1,4 @@
+// 1. Open cmd, and type => "執行檔位置(通常預設為下方這個路徑)" --dbpath="將把資料存放在哪個資料夾"
+    // ex : => "C:\Program Files\MongoDB\Server\4.0\bin\mongod.exe" --dbpath="c:\Code\MongoDB"
+// 2. Open new cmd, and type => mongo
+    // 這樣就進入 mongo shell 了
